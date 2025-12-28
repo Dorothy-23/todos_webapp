83ea005d45df7bf5e6f759598051afaffed3e737
dminayo <magulidorothy@gmail.com>
2025-12-28 11:19:38 +0300

    removed unwanted todos

--- webapp.py
+++ webapp.py
@@ -14,8 +14,8 @@ def add_todo():
 st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase productivity")#not more than 79 characters
-st.checkbox("Buy grocery")
-st.checkbox("Buy cars")
+#st.checkbox("Buy grocery")
+#st.checkbox("Buy cars")
 
 
 for index, todo in enumerate(todos):
